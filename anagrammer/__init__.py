6623814e67370ca6a06650077c4654aa96ac9d92
shaun <shaunhegarty@gmail.com>
2023-12-11 01:15:13 +0000

    Port endpoints over to fastapi
    - port models to sqlmodel/pydantic

--- anagrammer/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-# Initialize the app
-app = Flask(__name__, instance_relative_config=True)
-
-# pylint: disable=import-error,wrong-import-position
-from anagrammer import views
--- config/config.py
+++
@@ -1,3 +0,0 @@
-CONN_STRING = (
-    "dbname='words' user='api' password='letters' host='pgLettersApi' port=5432"
-)
